9161ba955ba2c993b8afa1671df8b0657361d99c
Joey Pettit <pettitbass@gmail.com>
2022-08-29 17:06:44 -0500

    finished third challenge

--- scripts/120-Arrays.js
+++ scripts/120-Arrays.js
@@ -3,8 +3,14 @@ console.log('THIS IS js120');
 let arr = ["ASDF", "BLAH", "Oh!", "Hi", "There!"];
 
 function loopsArrEtc(array, num){
-    for( let i = num; i = array.length; i++){
-        let finishedString
-        array[i] += finishedString;
+    let finishedString ="";
+    for( let i = num; i < array.length; i++){
+        finishedString += array[i];
+        finishedString += " ";
     }
+    return finishedString;
+    
 }
+
+console.log('Example 1:', loopsArrEtc(arr, 3));
+console.log('Example 2:', loopsArrEtc(arr, 2));
